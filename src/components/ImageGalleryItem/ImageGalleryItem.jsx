eeea098778b683fc527e73531f4248ec120636de
kait-meshcheriakova <129512123+kait-meshcheriakova@users.noreply.github.com>
2023-10-15 00:39:49 +0300

    change react-modal on nativ modal

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,30 +1,16 @@
 import React, { Component } from 'react';
 import { Img, Item } from './ImageGalleryItem.styled';
-import Modal from 'react-modal';
-const customStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-    overflow: 'clip',
-  },
-};
+import Modal from '../Modal/Modal';
 
-Modal.setAppElement('#root');
 export class ImageItem extends Component {
   state = {
     isModalOpen: false,
   };
 
-  openModal = () => {
-    this.setState({ isModalOpen: true });
-  };
-
-  closeModal = () => {
-    this.setState({ isModalOpen: false });
+  toggleModal = () => {
+    this.setState(({ isModalOpen }) => ({
+      isModalOpen: !isModalOpen,
+    }));
   };
 
   render() {
@@ -35,16 +21,15 @@ export class ImageItem extends Component {
         <Img
           src={image.webformatURL}
           alt={image.tags}
-          onClick={this.openModal}
+          onClick={this.toggleModal}
         />
-        <Modal
-          isOpen={isModalOpen}
-          onRequestClose={this.closeModal}
-          style={customStyles}
-          contentLabel="Example Modal"
-        >
-          <img src={image.largeImageURL} alt={image.tags} />
-        </Modal>
+        {isModalOpen && (
+          <Modal
+            largeImageURL={image.largeImageURL}
+            tags={image.tags}
+            onClose={this.toggleModal}
+          />
+        )}
       </Item>
     );
   }
